a26b461a31e5735935743912ea7e1f8c729cd1a4
Trần Văn Thiện <napcard20kkk@gmail.com>
2026-09-25 14:40:43 +0700

    update 2

--- ThucHanh02/BaiTap1/Bai1_2/obj/Debug/net9.0/Bai1_2.AssemblyInfo.cs
+++ ThucHanh02/BaiTap1/Bai1_2/obj/Debug/net9.0/Bai1_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai1_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef1e6ff7455179ce175421d7a596fb46ffe264c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53393e34fb526fc3eae58d369c5d81d6322d33ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai1_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai1_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
